744355b64253294f1fb9c489ffd29270b14cc37d
Angad Singh <angad.w.s.1994@gmail.com>
2018-05-20 23:22:05 +0530

    Reduced FS calls

--- index.js
+++ index.js
@@ -10,18 +10,30 @@ const makeDir = () => new Promise((resolve, reject) => {
 
 makeDir()
   .then(() => {
-    fs.readFile("./index.html", "utf-8", (err, data) => {
-      lib.create("index.html", data, (err) => {
-        console.log(err);
-      });
+    lib.create("index.html",
+`<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+</head>
+<body>
+  <script src="main.js"></script>
+</body>
+</html>`, (err) => {
+      console.log(err);
     });
-    fs.readFile("./styles.css", "utf-8", (err, data) => {
-      lib.create("styles.css", data, (err) => {
-        console.log(err);
-      });
+    lib.create("styles.css",
+`body, html {
+  padding: 0;
+  margin: 0;
+}`, (err) => {
+      console.log(err);
     });
     lib.create('main.js', "", (err) => {
       console.log(err);
     });
   });
-
